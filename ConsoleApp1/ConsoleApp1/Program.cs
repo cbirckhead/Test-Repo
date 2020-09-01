3174922d351b9d4d73baebcc9d3ae969add965c1
cbirckhead <70603154+cbirckhead@users.noreply.github.com>
2020-09-01 15:41:13 -0400

    jf

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World! 12345");
+            Console.WriteLine("This is a test!");
             
             ;;
         }
